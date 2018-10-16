f9d0d40e4b32f6c5818a04038270123c8acfa21d
Chris Cardle <cdcardle@gmail.com>
2018-10-16 15:08:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function Scooter(year, color, model) {
-  this.year = year;
-  this.color = color;
-  this.model = model;
+function createThings(...params) {
+  params.forEach(param => this.param = param);
 }
+
+const Scooter = createThings(year, color, model);
+
+// function Scooter(year, color, model) {
+//   this.year = year;
+//   this.color = color;
+//   this.model = model;
+// }
+
+// function Scooter(year, color, model) {
+//   this.year = year;
+//   this.color = color;
+//   this.model = model;
+// }
